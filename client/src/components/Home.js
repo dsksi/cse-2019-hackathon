17fb4344c2ea14b3acca98dd12339f12fcf36267
Qiwei Yang <yangqiwei97@gmail.com>
2019-07-28 01:14:10 +1000

    add api calls

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,45 +6,100 @@ import axios from 'axios';
 export default class Home extends Component {
   constructor(props) {
 		super(props);
-		 this.state = { pictures: [] };
-		 this.onDrop = this.onDrop.bind(this);
+		 this.state = { 
+       pictures: [],
+       objects: [], // the object
+       objectsStr: "", // the object
+       classes: [], // classes
+       classesStr: "", // classes
+       displayButton: "block",
+       country: 0,
+       id: 0,
+      };
+     this.onDrop = this.onDrop.bind(this);
 	}
 
 	onDrop(pictureFiles, pictureDataURLs) {
 		this.setState({
-            pictures: this.state.pictures.concat(pictureFiles),
-        });
-        // console.log(this.state.pictureDataURLs);
-        this.predict();
+        pictures: this.state.pictures.concat(pictureFiles),
+    });
+    // console.log(this.state.pictureDataURLs);
+    if (pictureFiles.length >= 1) {
+      this.setState({
+        displayButton: "none",
+      });
+    } else {
+      this.setState({
+        displayButton: "block",
+      });
     }
+    if (pictureFiles.length > 0) {
+      this.setState({
+        objectsStr: "",
+        objects: [],
+      })
+      this.setState({
+        classesStr: "",
+        classes: [],
+      })
+      this.predict();
+    }
+
+  }
       
-    async predict() {
-      const image = document.getElementsByTagName('img');
-      console.log(image)
-      // Load the model.
-      const model = await cocoSsd.load();
-      // Classify the image.
-      const predictions = await model.detect(image[1]);
-      if (predictions.length === 0) {
+  async predict() {
+    const image = document.getElementsByTagName('img');
+    console.log(image)
+    // Load the model.
+    const model = await cocoSsd.load();
+    // Classify the image.
+    const predictions = await model.detect(image[1]); //
+
+    if (predictions.length === 0) {
+      this.setState({
+        type: 1,
+      })
+    } else {
+      // add to objects
+      predictions.forEach(p => {
         this.setState({
-          type: 1,
+          objectsStr: this.state.objectsStr.concat(p.class) + ' ',
+          objects: this.state.objects.concat(p.class),
         })
-      } else {
-        console.log('Predictions: ');
-        console.log(predictions);
-      }
+      });
+      console.log('Predictions: ');
+      console.log(this.state.objectsStr);
+      console.log(predictions);
     }
+
+    // get class
+    for (let i = 0; i< this.state.objects.length; i++) {
+      let obj = this.state.objects[i];
+      axios.get(`https://littlelitter.herokuapp.com/country/${this.state.country}/label/${obj}`)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  }
 
     render() {
         return (
+          <div>
+            <h1>Object: {this.state.objectsStr} </h1>
+            <h1>Classes: {this.state.classesStr} </h1>
             <ImageUploader
                 	withIcon={true}
                 	buttonText='Choose images'
                 	onChange={this.onDrop}
-                	imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                	imgExtension={['.jpg', '.jpeg', '.gif', '.png', '.gif']}
                   maxFileSize={5242880}
                   withPreview={true}
+                  buttonStyles={{ display: this.state.displayButton }}
             />
+          </div>
         );
     }
 }
