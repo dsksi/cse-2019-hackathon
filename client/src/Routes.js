3b86bf3bff625121dc57bb69074e22aef8f3de20
Queenie <jiyuqi0310@gmail.com>
2019-07-27 17:43:06 +1000

    upload image

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Country from "./components/Country";
+import zoom from "./components/zoom";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/country" exact component={Country} />
+    <Route path="/zoom" exact component={zoom} />
   </Switch>;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,31 +1,25 @@
 import React, { Component } from "react";
-import ImageUploader from 'react-images-upload';
+// import ImageUploader from 'react-images-upload';
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
-     this.state = { pictures: [] };
-     this.onDrop = this.onDrop.bind(this);
-  }
-
-  onDrop(picture) {
+  constructor(props){
+    super(props)
+    this.state = {
+      file: null
+    }
+  }  
+  handleChange=(event)=> {
     this.setState({
-        pictures: this.state.pictures.concat(picture),
-    });
-}
+      file: URL.createObjectURL(event.target.files[0])
+    })
+  }  
   render() {
     return (
       <div>
-        <h1>Little litters!!!</h1>
-        <p>upload image here</p>
-        <ImageUploader
-            withIcon={true}
-            withPreview={true}
-            buttonText='Choose images'
-            onChange={this.onDrop}
-            imgExtension={['.jpg', '.gif', '.png', '.gif']}
-            maxFileSize={5242880}
-        />
+        <input type="file" onChange={this.handleChange}/>
+
+        <img src={this.state.file}/>
+        
       </div>
     );
   }
