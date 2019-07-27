22fe126302cb2f56170ac67fbb8cbb22dc6b3c88
Qiwei Yang <yangqiwei97@gmail.com>
2019-07-27 20:46:12 +1000

    remove extra lines

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,61 +4,6 @@ import * as cocoSsd from '@tensorflow-models/coco-ssd'
 import axios from 'axios';
 
 export default class Home extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { 
-  //     picture: null,
-  //     tempData: {},
-  //     type: null,
-  //     disabledButton: "block"
-  //   };
-  // }
-
-  // onDrop = (event) => {
-
-  //   this.setState({
-  //       picture: URL.createObjectURL(event.target.files[0]),
-  //       disabledButton: "none",
-  //   });
-    
-  //   this.predict();
-  // }
-
-  // async predict() {
-  //   const image = document.getElementById('image');
-
-  //   // Load the model.
-  //   const model = await cocoSsd.load();
-  //   // Classify the image.
-  //   const predictions = await model.detect(image);
-  //   if (predictions.length === 0) {
-  //     this.setState({
-  //       type: 1,
-  //     })
-  //   } else {
-  //     console.log('Predictions: ');
-  //     console.log(predictions);
-  //   }
-
-  //   this.setState({
-  //     disabledButton: "block",
-  //   })
-  // }
-
-  // render() {
-  //   let width = window.outerWidth * 0.8;
-  //   // console.log(window.outerWidth)
-  //   return (
-  //     <div>
-  //       { this.state.type === 1 && <h2>Sorry, we can't recognise.</h2> }
-  //       <h1>Little litters!!!</h1>
-  //       <p>upload image here</p>
-  //       <input style={{ display: this.state.disabledButton }} type="file" onChange={this.onDrop}/>
-  //       <img id="image" alt="no uploaded images" width={width} src={this.state.picture}/>
-  //     </div>
-  //   );
-  // }
-
   constructor(props) {
 		super(props);
 		 this.state = { pictures: [] };
@@ -88,10 +33,6 @@ export default class Home extends Component {
         console.log('Predictions: ');
         console.log(predictions);
       }
-  
-      this.setState({
-        disabledButton: "block",
-      })
     }
 
     render() {
